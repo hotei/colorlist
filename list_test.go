137819b26db9b06705d6eb2f4b0d52d197644096
Dave Chapeskie <dchapes@github.com>
2013-12-10 09:36:26 -0500

    Fix tests to actually be tests and not spew with fmt.

--- list_test.go
+++ list_test.go
@@ -9,77 +9,110 @@
 package colorlist
 
 import (
-	"fmt"
 	"image/color"
+	"strings"
 	"testing"
 )
 
 func Test_0002(t *testing.T) {
-	fmt.Printf("\nTest_0002\n")
-
-	s1 := "#ccc"
-	s1b := "#cCc"
-
-	s2 := "#0c0c0c"
-	s2b := "#0c0C0c"
-
-	s3 := "#0c0Z0c"
-
-	// convert to rgba {12,12,12,255}
-	// TODO(mdr): make test use t.Errorf if results don't work out right
-	fmt.Printf("Everything should convert to { 12, 12, 12, 255 }\n")
-	fmt.Printf(" %s to color.RGBA = %v\n", s1, HexToColorRGBA(s1))
-	fmt.Printf(" %s to color.RGBA = %v\n", s1b, HexToColorRGBA(s1b))
-	fmt.Printf(" %s to color.RGBA = %v\n", s2, HexToColorRGBA(s2))
-	fmt.Printf(" %s to color.RGBA = %v\n", s2b, HexToColorRGBA(s2b))
-	fmt.Printf("Next one should fail and return default of black {0,0,0,255}\n")
-	fmt.Printf(" %s to color.RGBA = %v\n", s3, HexToColorRGBA(s3))
+	grey := color.RGBA{12, 12, 12, 255}
+	black := color.RGBA{0, 0, 0, 255}
+	for _, d := range []struct {
+		string
+		color.RGBA
+	}{
+		{"#ccc", grey},
+		{"#ccc", grey},
+		{"#cCc", grey},
+		{"#0c0c0c", grey},
+		{"#0c0C0c", grey},
+		{"#0c0Z0c", black},
+	} {
+		_ = d
+		c := HexToColorRGBA(d.string)
+		if c != d.RGBA {
+			t.Errorf("HexToColorRGBA(%q) returned %v, expected %v",
+				d.string, c, d.RGBA)
+		} else {
+			//t.Logf("HexToColorRGBA(%q) returned %v, as expected", d.string, c)
+		}
+	}
 }
 
 func Test_0001(t *testing.T) {
-	fmt.Printf("\nTest_0001\n")
-
-	if ColorName(color.RGBA{245, 245, 245, 255}) != "whitesmoke" {
-		t.Errorf("ColorName failed for Whitesmoke\n")
+	whitesmoke := color.RGBA{245, 245, 245, 255} // whitesmoke
+	for _, d := range []struct {
+		color.RGBA
+		name, nearest string
+	}{
+		{whitesmoke, "whitesmoke", "whitesmoke"},
+		{color.RGBA{244, 244, 240, 255}, "", "whitesmoke"},
+		{color.RGBA{0, 0, 0, 255}, "black", "black"},
+		{color.RGBA{0, 0, 255, 255}, "blue", "blue"},
+		{color.RGBA{0, 0, 231, 250}, "", "blue"},
+		{color.RGBA{0, 0, 231, 5}, "", "blue"},
+		{color.RGBA{0, 0, 230, 250}, "", "blue"},
+		{color.RGBA{0, 0, 229, 250}, "", "mediumblue"},
+		{color.RGBA{0, 0, 229, 5}, "", "mediumblue"},
+	} {
+		if g, e := ColorName(d.RGBA), d.name; g != e {
+			t.Errorf("ColorName(%v) returned %q, expected %q", d.RGBA, g, e)
+		} else {
+			//t.Logf("ColorName(%v) returned %q, as expected", d.RGBA, g)
+		}
+		if g, e := ColorNameNearest(d.RGBA), d.nearest; g != e {
+			t.Errorf("ColorNameNearest(%v) returned %q, expected %q", d.RGBA, g, e)
+		} else {
+			//t.Logf("ColorNameNearest(%v) returned %q, as expected", d.RGBA, g)
+		}
 	}
 
-	fmt.Printf("ColorName({245,245,245,255}) = %s (should be whitesmoke)\n",
-		ColorName(color.RGBA{245, 245, 245, 255}))
-
-	fmt.Printf("NearestColorName({244,244,240,255}) = %s (should be whitesmoke)\n",
-		ColorNameNearest(color.RGBA{244, 244, 240, 255}))
-
-	if ColorNameNearest(color.RGBA{244, 244, 240, 255}) != "whitesmoke" {
-		t.Errorf("NearestColorName failed\n")
+	black := color.RGBA{0, 0, 0, 255}
+	for _, d := range []struct {
+		color.RGBA
+		name string
+	} {
+		{color.RGBA{255,102,204,255}, "radish"},
+	} {
+		if g, e := ColorVal(d.name), black; g != e {
+			t.Errorf("ColorVal(%q) returned %v, expected %v", d.name, g, e)
+		} else {
+			t.Logf("ColorVal(%q) returned %v, as expected", d.name, g)
+		}
+		//AddColor("radish", 255, 102, 204, 255)
+		r, g, b, a := d.RGBA.RGBA()
+		AddColor(d.name, int(r), int(g), int(b), int(a))
+		if g, e := ColorVal(d.name), d.RGBA; g != e {
+			t.Errorf("ColorVal(%q) returned %v, expected %v", d.name, g, e)
+		} else {
+			//t.Logf("ColorVal(%q) returned %v, as expected", d.name, g)
+		}
 	}
 
-	ws := color.RGBA{245, 245, 245, 255} // whitesmoke
-	if ColorVal("WhiteSmoke") != ws {    // note CamelCase isn't how it was added
-		t.Errorf("ColorVal(string) failed got %v, not {244,244,240,255} \n", ColorVal("WhiteSmoke"))
-	}
-
-	AddColor("radish", 255, 102, 204, 255)
-
-	//ws = ColorVal("#ccc")
-	newSmoke := ColorVal("#f5f5F5")
-	fmt.Printf("Testing newSmoke = #f5f5F5\n")
-	if newSmoke != ws {
-		t.Errorf("ColorVal(#string) failed got %v, not {244,244,240,255} \n", newSmoke)
-	}
-
-	svgSmoke := SVGColorStr("whitesmoke")
-	fmt.Printf("svg name of whitesmoke should be #f5f5f5, got %s\n", svgSmoke)
-	if svgSmoke != "#f5f5f5" {
-		t.Errorf("svg name of whitesmoke should be #f5f5f5, not %s\n", svgSmoke)
-	}
-	svgSmoke = SVGColorStr("black")
-	fmt.Printf("svg name of black should be #000000, got %s\n", svgSmoke)
-	if svgSmoke != "#000000" {
-		t.Errorf("svg name of black should be #000000, not %s\n", svgSmoke)
-	}
-	svgSmoke = SVGColorStr("radish")
-	fmt.Printf("svg name of radish should be #ff66cc, got %s\n", svgSmoke)
-	if svgSmoke != "#ff66cc" {
-		t.Errorf("svg name of radish should be #ff66cc, not %s\n", svgSmoke)
+	for _, d := range []struct {
+		color.RGBA
+		name, nearest, svg string
+	}{
+		{whitesmoke, "whitesmoke", "whitesmoke", "#f5f5f5"},
+		{color.RGBA{0, 0, 0, 255}, "black", "black", "#000000"},
+		{color.RGBA{0, 0, 255, 255}, "blue", "blue", "#0000ff"},
+		{color.RGBA{255,102,204,255}, "radish", "radish", "#ff66cc"},
+	} {
+		nm := strings.Title(d.name)
+		if g, e := ColorVal(nm), d.RGBA; g != e {
+			t.Errorf("ColorVal(%q) returned %v, expected %v", nm, g, e)
+		} else {
+			//t.Logf("ColorVal(%q) returned %v, as expected", nm, g)
+		}
+		if g, e := SVGColorStr(d.name), d.svg; g != e {
+			t.Errorf("SVGColorStr(%q) returned %v, expected %v", d.name, g, e)
+		} else {
+			//t.Logf("SVGColorStr(%q) returned %v, as expected", d.name, g)
+		}
+		if g, e := ColorVal(d.svg), d.RGBA; g != e {
+			t.Errorf("ColorVal(%q) returned %v, expected %v", d.svg, g, e)
+		} else {
+			//t.Logf("ColorVal(%q) returned %v, as expected", d.svg, g)
+		}
 	}
 }
